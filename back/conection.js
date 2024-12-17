81da1a0d30c6b9b4419238bd59f5626eef9be76d
117Castro <131223103+117Castro@users.noreply.github.com>
2024-12-17 11:43:17 -0600

    implementacion de nuevas funciones
    funciones agregadas:
    
    implementacion del localstorage.
    implementaccion de ventana modal.

--- back/conection.js
+++ back/conection.js
@@ -8,13 +8,49 @@ function loguear(){
 
         window.location="Page_aterrisage.html";
     } else {
-        alert("Usuaro o contraseña incorrecta");
+        //alert("Usuaro o contraseña incorrecta");
+        openmodal();
     }
 
+    //almacena la informacion 
+    localStorage.setItem( "usuario", usuario );
 
+   
 }
+
+
 
 function closer(){
     window.location="login.html";
     localStorage.clear();
 }
+
+
+
+function openmodal(){
+
+    let entermodal= document.getElementById("btnlogin");
+
+    entermodal.addEventListener("click", function(){
+        modal.showModal();
+    })
+}
+
+function closemodal(){
+    let cancelButton= document.getElementById("cerrar_modal");
+
+    cancelButton.addEventListener("click", function () {
+      modal.close();
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
