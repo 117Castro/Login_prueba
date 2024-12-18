99ce58f0f779f4cbe998ab28735247f786dc1d0e
117Castro <131223103+117Castro@users.noreply.github.com>
2024-12-17 21:44:04 -0600

    MOdificaciones de prueba
    estilisado de nueva ventada de modal, y prueba de eficiencia de funciones.

--- back/conection.js
+++ back/conection.js
@@ -1,17 +1,22 @@
 
 function loguear(){
 
+    
+    
     let usuario = document.getElementById("user_correo").value;
     let contra = document.getElementById("password").value;
-
+    
     if (usuario=== "user@gmail.com" && contra === "#Somosdev"){
-
+        
         window.location="Page_aterrisage.html";
     } else {
-        //alert("Usuaro o contraseña incorrecta");
         openmodal();
+    
+       
+    
+    
     }
-
+    
     //almacena la informacion 
     localStorage.setItem( "usuario", usuario );
 
